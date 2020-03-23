89b7f057f12effd7881bf5ea35908472c90ac7d8
Miro Phan <phanmir@gmail.com>
2020-03-23 01:46:59 +0100

    implement docker service via Docker SDK for Python

--- bescape/_deconv.py
+++ bescape/_deconv.py
@@ -1,7 +1,7 @@
 import os
 from tempfile import TemporaryDirectory
 from shutil import copy
-from spython.main import Client
+
 
 dirname = os.path.dirname(__file__)
 
@@ -10,12 +10,47 @@ PATH_DOCKER = os.path.join(dirname, '')
 
 
 class Bescape:
-    img_dir_input = ':/app/input'
-    img_dir_output = ':/app/output'
-    img_dir_gep = ':/app/gep'
+    """BESCApe - a cell deconvolution class
 
-    def __init__(self):
-        Client.load(PATH_SINGULARITY)
+    The deconvolution classifier is packed in a container. This class is a wrapper
+    that uses either Docker or Singularity as a service to run containers.
+
+    Args:
+        service (str): One of ['docker', 'singularity']. Determine which service to use
+        dockerhub_image (str): Only for Docker. Provide the url to the BESCApe deconvolution image on DockerHub.
+        path_singularity (str): Only for Singularity. Provide the absolute path to the Singularity container file for BESCApe.
+    """
+    img_dir_input = '/app/input'
+    img_dir_output = '/app/output'
+    img_dir_gep = '/app/gep'
+
+    def __init__(self, service='docker', dockerhub_image='phanmir/bescape', path_singularity=PATH_SINGULARITY):
+        self.service = service
+        self.dockerhub_image = dockerhub_image
+        self.path_singularity = path_singularity
+
+        if self.service == 'docker':
+            try:
+                import docker
+                self.client = docker.from_env()
+                self.client.images.pull(dockerhub_image)
+                print('Docker client instantiated')
+                print('Docker image loaded: ', dockerhub_image)
+
+            except ModuleNotFoundError:
+                print('Python library for the Docker Engine API not installed')
+        elif self.service == 'singularity':
+            try:
+                from spython.main import Client
+                Client.load(PATH_SINGULARITY)
+                print('Singularity client loaded')
+                print('Singularity container loaded: ', PATH_SINGULARITY)
+
+            except ModuleNotFoundError:
+                print('Python library for the Singularity API not installed')
+        else:
+            raise ValueError(
+                "Selected platform not supported. Chooes either Docker or Singularity")
 
     def deconvolute_gep(self,
                         file_gep,
@@ -23,17 +58,18 @@ class Bescape:
                         dir_output,
                         method='bescape'):
         """Perform deconvolution using a Gene expression profile as the basis vector
-        Calls a Singularity/Docker container which contains the required Python and R dependencies 
-        for all supported deconvolution methods. 
+
+        Calls a Singularity/Docker container which contains the required Python and R dependencies
+        for all supported deconvolution methods.
 
         Args:
-            file_gep (.csv): path to the gene expression profile .csv file. First column has to be 
-                ENSEMBL gene names, first row are cell type labels. Gene expression should be linear, 
-                not log-transformed. 
-            dir_input (String): absolute path to the directory  containing the input.csv file. 
+            file_gep (.csv): path to the gene expression profile .csv file. First column has to be
+                ENSEMBL gene names, first row are cell type labels. Gene expression should be linear,
+                not log-transformed.
+            dir_input (String): absolute path to the directory  containing the input.csv file.
                 The correct format of input.csv is described in the tutorial.
             dir__output (String): absolute path for desired output directory
-            method (String): desired method to be used for deconvolution. 
+            method (String): desired method to be used for deconvolution.
                 Current options: ['bescape', 'epic', 'music', 'scdc']
 
         Returns:
@@ -59,29 +95,56 @@ class Bescape:
                        dir_output,
                        method='music'):
         """Perform deconvolution using single cell (sc) annotations as the basis vector.
-        User provides single cell annotations (either as scanpy AnnData obj or as eset RDS obj). 
+
+        User provides single cell annotations (either as scanpy AnnData obj or as eset RDS obj).
         The deconvolution method will generate its own GEP based on the provided annotations.
 
         Args:
-            dir_annot (String) && method=='music': absolute path to the directory  containing only 
-                ONE cell annotation file. This should either be an ExpressionSet class saved as .RDS, 
-                or an AnnData object saved as .h5ad. If AnnData is provided, it will be converted to 
+            dir_annot (String) && method=='music': absolute path to the directory  containing only
+                ONE cell annotation file. This should either be an ExpressionSet class saved as .RDS,
+                or an AnnData object saved as .h5ad. If AnnData is provided, it will be converted to
                 an ExpressionSet.
             dir_annot (String) && method=='scdc': absolute path to the directory  containing MULTIPLE
-                cell annotation files. These should either be an ExpressionSet class saved as .RDS, 
-                or an AnnData object saved as .h5ad. If AnnData is provided, it will be converted to 
+                cell annotation files. These should either be an ExpressionSet class saved as .RDS,
+                or an AnnData object saved as .h5ad. If AnnData is provided, it will be converted to
                 an ExpressionSet.
 
         """
 
-        if method == 'music':
-                dir_gep = os.path.join(dir_annot, self.img_dir_gep)
-                dir_input = os.path.join(dir_input, self.img_dir_input)
-                dir_output = os.path.join(dir_output, self.img_dir_output)
+        if not os.path.exists(dir_output):
+            os.makedirs(dir_output)
+        if self.service == 'docker':
+            vol_dict = {dir_annot.rstrip('/'): {'bind': self.img_dir_gep, 'mode': 'ro'},
+                        dir_input.rstrip('/'): {'bind': self.img_dir_input, 'mode': 'ro'},
+                        dir_output.rstrip('/'): {'bind': self.img_dir_output, 'mode': 'rw'}}
+
+            if method == 'music':
+
+                c = self.client.containers.run(
+                    self.dockerhub_image, command='python3 musicpy.py', volumes=vol_dict, detach=True)
+                for line in c.logs(stream=True):
+                    print(line.decode().strip())
+
+            elif method == 'scdc':
+                c = self.client.containers.run(
+                    self.dockerhub_image, command='python3 scdcpy.py', volumes=vol_dict, detach=True)
+                for line in c.logs(stream=True):
+                    print(line.decode().strip())
+            else:
+                raise ValueError("Selected method not supported: %s" % method)
+
+        elif self.service == 'singularity':
+            if method == 'music':
+                dir_gep = os.path.join(dir_annot.rstrip(
+                    '/'), ':/', self.img_dir_gep.lstrip('/'))
+                dir_input = os.path.join(dir_input.rstrip(
+                    '/'), ':/', self.img_dir_input.lstrip('/'))
+                dir_output = os.path.join(dir_output.rstrip(
+                    '/'), ':/', self.img_dir_output.lstrip('/'))
                 param_bind = [dir_input, dir_output, dir_gep]
                 cmd = ['python3', '/app/musicpy.py']
                 Client.execute(cmd, bind=param_bind)
-        elif method == 'scdc':
+            elif method == 'scdc':
                 dir_gep = os.path.join(dir_annot, self.img_dir_gep)
                 dir_input = os.path.join(dir_input, self.img_dir_input)
                 dir_output = os.path.join(dir_output, self.img_dir_output)
