26c8ffc61a79e0f8879394f5797672e5824f9cfd
Miro Phan <phanmir@gmail.com>
2020-04-11 16:12:50 +0200

    set license to gpl3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GNU General Public License v3.0',   # Again, pick a license
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3),'
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
