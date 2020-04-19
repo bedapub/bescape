f62a03b85e9fdcb2d7b224db092fd3034d570503
Miro Phan <phanmir@gmail.com>
2020-04-19 18:06:54 +0200

    add epic

--- bescape/docker_files/epic.R
+++ bescape/docker_files/epic.R
@@ -1,25 +1,44 @@
-p <- .packages(all.available = TRUE)
-if (!("EPIC" %in% p)) {
-  if (!("devtools" %in% p)) {
-    install.packages("devtools")
-  }
-  devtools::install_github("GfellerLab/EPIC")
-}
-rm(p)
-
-library("EPIC")
-
+suppressMessages(library("EPIC"))
+suppressMessages(library("Biobase"))
 # python handler checks for proper input of arguments
 myArgs <- commandArgs(trailingOnly = TRUE)
 
-mydata <- read.table(myArgs[1], row.names=1, header=TRUE)
+#mydata <- read.table(myArgs[1], row.names=1, header=TRUE)
+path_gep <- myArgs[1]
+path_bulk <- myArgs[2]
+name_temp_file <- myArgs[3]
+path_temp_file <-  paste('./', name_temp_file, sep='')
+
+# load bulk
+bulk <- readRDS(file=path_bulk)
+
+if(class(bulk)[1]=="DGEList") {
+    print("Converting DGEList class to eset")
+    b.eset <- ganalyse::as.eset(bulk)
+    b.eset <- exprs(b.eset)
+    print("Setting Gene Symbols as featureNames")
+    featureNames(b.eset) <- b.eset@featureData@data$GeneSymbol
+} else if (class(bulk)[1]=="ExpressionSet"){
+    b.eset <- exprs(bulk)
+} else {
+    warning("Error: input bulk dataset not an ExpressionSet or DGEList")
+}
+
 
 tryCatch(
-  {
-    out <- EPIC(mydata)
-    write.table(out$cellFractions, myArgs[2], sep="	")
-  },
-  error = function(e) {
-    return(paste("Error:",e))
-  }
+    {
+        print('Performing deconvolution using EPIC')
+        if (path_gep=='epic'){
+            out <- EPIC(b.eset)
+            write.table(out$cellFractions, path_temp_file, sep="\t")
+        } else {
+            gep.eset <- readRDS(path_gep)
+            gep.epic <- list(refProfiles=exprs(gep.eset), sigGenes='')
+            out <- EPIC(b.eset, gep.epic)
+            write.table(out$cellFractions, path_temp_file, sep="\t")
+        }
+    },
+    error = function(e) {
+        return(paste("Error:",e))
+    }
 )
---
+++ bescape/docker_files/epicpy_ref.py
@@ -0,0 +1,39 @@
+import os
+import pandas as pd
+import subprocess
+from _helper_deconv import export_predictions, get_gep_eset_file, get_input_eset_file
+
+
+def music(gep, bulk_rna, sep='\t'):
+    """R wrapper function. Uses subprocess to call shell commands form python. Calls script_music.R file
+        to deconvolute. 
+
+        The input files are R classes. The user will either have directly provided these, or they will have
+        been generated from AnnData files by bescape conversion functions.
+    Args:
+        gep (eset): eset expression file (R class) containing single cell annotations that must come 
+        from at least two different subjects for music to build its own GEP
+        bulk_rna (eset): eset expression file (R class) with bulk RNA expressions. Can be one or multiple samples
+        to be deconvoluted.
+    """
+    if not os.path.isfile(gep):
+        raise FileNotFoundError(
+            'Gene expression profile '+gep+' cannot be found.')
+    if not os.path.isfile(bulk_rna):
+        raise FileNotFoundError(
+            'Bulk RNA file ' + bulk_rna + ' cannot be found.')
+    cmd = 'Rscript /app/epic.R ' + 'epic'  + ' ' + bulk_rna + ' temp.txt'
+    p = subprocess.run([cmd], shell=True, stdout=None, stderr=None)
+
+    out = pd.read_csv('temp.txt', sep='\t', header=0, index_col=0)
+    os.remove('temp.txt')
+
+    print('Deconvolution using MuSiC finished successfully')
+
+    return out
+
+
+dir_input = get_input_eset_file()
+
+pred = music(gep=file_annot, bulk_rna=dir_input)
+export_predictions(pred_df=pred, header=True, index=True)
